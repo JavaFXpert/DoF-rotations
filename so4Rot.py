92aa6356124e779dfacf28a3db94dccb252fbb27
JavaFXpert <james@j1w.xyz>
2018-05-22 20:39:32 +0200

    Y rotation of all six degrees of freedom, with expected results

--- so4Rot.py
+++ so4Rot.py
@@ -37,9 +37,9 @@ p.inst(ACRX(np.pi/4)(1, 0))
 p.inst(CNOT(0, 1))
 
 # DE rotation
-p.inst(CNOT(0, 1))
-p.inst(CRX(np.pi/4)(1, 0))
-p.inst(CNOT(0, 1))
+p.inst(CNOT(1, 0))
+p.inst(CRX(np.pi/4)(0, 1))
+p.inst(CNOT(1, 0))
 
 # DF rotation
 p.inst(CRX(np.pi/4)(0, 1))
@@ -47,12 +47,17 @@ p.inst(CRX(np.pi/4)(0, 1))
 # EF rotation
 p.inst(CRX(np.pi/4)(1, 0))
 
-
-
 wavefunction = qvm.wavefunction(p)
 print(wavefunction)
 
-
-
-
-
+# Corresponding rotations in Julia Language
+# matrix = eye(4)
+## rotMatrix = eye(4)
+# r1=r2=r3=r4=r5=r6=22.5
+# rotMatrix = transpose([cosd(r1) -sind(r1) 0 0; sind(r1) cosd(r1) 0 0; 0 0 1 0; 0 0 0 1]) *
+# transpose([cosd(r2) 0 -sind(r2) 0; 0 1 0 0; sind(r2) 0 cosd(r2) 0; 0 0 0 1]) *
+# transpose([cosd(r3) 0 0 -sind(r3); 0 1 0 0; 0 0 1 0; sind(r3) 0 0 cosd(r3)]) *
+# transpose([1 0 0 0; 0 cosd(r4) -sind(r4) 0; 0 sind(r4) cosd(r4) 0; 0 0 0 1]) *
+# transpose([1 0 0 0; 0 cosd(r5) 0 -sind(r5); 0 0 1 0; 0 sind(r5) 0 cosd(r5)]) *
+# transpose([1 0 0 0; 0 1 0 0; 0 0 cosd(r6) -sind(r6); 0 0 sind(r6) cosd(r6)]) *
+# matrix
