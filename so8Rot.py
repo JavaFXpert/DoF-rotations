aa3f677241651a83c2f7d5e9e74fe95900bc12d3
JavaFXpert <james@j1w.xyz>
2018-05-23 12:37:36 +0200

    Applying all rotations matches expected results.

--- so8Rot.py
+++ so8Rot.py
@@ -27,7 +27,7 @@ ccry = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, quil_cos(theta / 2), -1 * quil_sin(theta / 2)],
                 [0, 0, 0, 0, 0, 0, quil_sin(theta / 2), quil_cos(theta / 2)]])
 
-#NOT QUITE RIGHT?
+#TODO: Ascertain what kind of gates this is?
 cary = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0, 0, 0],
                 [0, 0, 1, 0, 0, 0, 0, 0],
@@ -56,86 +56,81 @@ p.inst(dg_ccry)
 p.inst(dg_cary)
 
 p.inst(X(0))
-# p.inst(X(1))
+p.inst(X(1))
 p.inst(X(2))
 
 # CD rotation
-# p.inst(AARY(np.pi/4)(2, 1, 0))
+p.inst(AARY(np.pi/4)(2, 1, 0))
 
 # CE rotation
-# p.inst(AARY(np.pi/4)(2, 0, 1))
+p.inst(AARY(np.pi/4)(2, 0, 1))
 
 # CF rotation
-# p.inst(CNOT(1, 0))
-# p.inst(AARY(np.pi/4)(0, 2, 1))
-# p.inst(CNOT(1, 0))
+p.inst(CNOT(1, 0))
+p.inst(AARY(np.pi/4)(0, 2, 1))
+p.inst(CNOT(1, 0))
 
 # CG rotation
-# p.inst(AARY(np.pi/4)(0, 1, 2))
+p.inst(AARY(np.pi/4)(0, 1, 2))
 
 # CA rotation
-# p.inst(CNOT(2, 0))
-# p.inst(AARY(np.pi/4)(0, 1, 2))
-# p.inst(CNOT(2, 0))
+p.inst(CNOT(2, 0))
+p.inst(AARY(np.pi/4)(0, 1, 2))
+p.inst(CNOT(2, 0))
 
 # CB rotation
-# p.inst(CNOT(2, 1))
-# p.inst(AARY(np.pi/4)(0, 1, 2))
-# p.inst(CNOT(2, 1))
+p.inst(CNOT(2, 1))
+p.inst(AARY(np.pi/4)(0, 1, 2))
+p.inst(CNOT(2, 1))
 
 # CC' rotation
-# p.inst(CNOT(1, 0))
-# p.inst(CNOT(1, 2))
-# p.inst(AARY(np.pi/4)(0, 2, 1))
-# p.inst(CNOT(1, 2))
-# p.inst(CNOT(1, 0))
+p.inst(CNOT(1, 0))
+p.inst(CNOT(1, 2))
+p.inst(AARY(np.pi/4)(0, 2, 1))
+p.inst(CNOT(1, 2))
+p.inst(CNOT(1, 0))
 
 # DE rotation
-# p.inst(CNOT(1, 0))
-# p.inst(CRY(np.pi/4)(0, 1))
-# p.inst(CNOT(1, 0))
+p.inst(ANOT(1, 0))
+p.inst(AARY(np.pi/4)(0, 2, 1))
+p.inst(ANOT(1, 0))
 
 # DF rotation
-# p.inst(X(2))
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
-# p.inst(X(2))
-
-# DA rotation
-# p.inst(X(1))
-# p.inst(CCRY(np.pi/4)(0, 1, 2))
-# p.inst(X(1))
-
-# DB rotation
-# p.inst(CNOT(1, 0))
-# p.inst(ANOT(1, 2))
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
-# p.inst(ANOT(1, 2))
-# p.inst(CNOT(1, 0))
-
-# DE rotation
-# p.inst(ANOT(1, 0))
-# p.inst(AARY(np.pi/4)(0, 2, 1))
-# p.inst(ANOT(1, 0))
+p.inst(X(2))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(X(2))
 
 # DG rotation
-# p.inst(ANOT(2, 0))
-# p.inst(AARY(np.pi/4)(0, 1, 2))
-# p.inst(ANOT(2, 0))
+p.inst(ANOT(2, 0))
+p.inst(AARY(np.pi/4)(0, 1, 2))
+p.inst(ANOT(2, 0))
+
+# DA rotation
+p.inst(X(1))
+p.inst(CCRY(np.pi/4)(0, 1, 2))
+p.inst(X(1))
+
+# DB rotation
+p.inst(CNOT(1, 0))
+p.inst(ANOT(1, 2))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(ANOT(1, 2))
+p.inst(CNOT(1, 0))
 
 # DC' rotation
-# p.inst(ANOT(1, 2))
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
-# p.inst(ANOT(1, 2))
+p.inst(ANOT(1, 2))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(ANOT(1, 2))
 
 # EF rotation
-# p.inst(X(2))
-# p.inst(CCRY(np.pi/4)(1, 2, 0))
-# p.inst(X(2))
+p.inst(X(2))
+p.inst(CCRY(np.pi/4)(1, 2, 0))
+p.inst(X(2))
 
 # EG rotation
-# p.inst(ANOT(2, 1))
-# p.inst(AARY(np.pi/4)(0, 1, 2))
-# p.inst(ANOT(2, 1))
+p.inst(ANOT(2, 1))
+p.inst(AARY(np.pi/4)(0, 1, 2))
+p.inst(ANOT(2, 1))
 
 # EA rotation
 p.inst(CNOT(0, 1))
@@ -145,56 +140,56 @@ p.inst(ANOT(0, 2))
 p.inst(CNOT(0, 1))
 
 # EB rotation
-# p.inst(X(0))
-# p.inst(CCRY(np.pi/4)(0, 1, 2))
-# p.inst(X(0))
+p.inst(X(0))
+p.inst(CCRY(np.pi/4)(0, 1, 2))
+p.inst(X(0))
 
 # EC' rotation
-# p.inst(ANOT(0, 2))
-# p.inst(CCRY(np.pi/4)(1, 2, 0))
-# p.inst(ANOT(0, 2))
+p.inst(ANOT(0, 2))
+p.inst(CCRY(np.pi/4)(1, 2, 0))
+p.inst(ANOT(0, 2))
 
 # FG rotation
-# p.inst(CARY(np.pi/4)(2, 1, 0))
+p.inst(CARY(np.pi/4)(2, 1, 0))
 
 # FA rotation
-# p.inst(CNOT(2, 1))
-# p.inst(CCRY(np.pi/4)(0, 1, 2))
-# p.inst(CNOT(2, 1))
+p.inst(CNOT(2, 1))
+p.inst(CCRY(np.pi/4)(0, 1, 2))
+p.inst(CNOT(2, 1))
 
 # FB rotation
-# p.inst(CNOT(2, 0))
-# p.inst(CCRY(np.pi/4)(0, 1, 2))
-# p.inst(CNOT(2, 0))
+p.inst(CNOT(2, 0))
+p.inst(CCRY(np.pi/4)(0, 1, 2))
+p.inst(CNOT(2, 0))
 
 # FC' rotation
-# p.inst(CCRY(np.pi/4)(0, 1, 2))
+p.inst(CCRY(np.pi/4)(0, 1, 2))
 
 # GA rotation
-# p.inst(X(1))
-# p.inst(CCRY(np.pi/4)(1, 2, 0))
-# p.inst(X(1))
+p.inst(X(1))
+p.inst(CCRY(np.pi/4)(1, 2, 0))
+p.inst(X(1))
 
 # GB rotation
-# p.inst(X(0))
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
-# p.inst(X(0))
+p.inst(X(0))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(X(0))
 
 # GC' rotation
-# p.inst(ANOT(1, 0))
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
-# p.inst(ANOT(1, 0))
+p.inst(ANOT(1, 0))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(ANOT(1, 0))
 
 # AB rotation
-# p.inst(CNOT(1, 0))
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
-# p.inst(CNOT(1, 0))
+p.inst(CNOT(1, 0))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(CNOT(1, 0))
 
 # AC' rotation
-# p.inst(CCRY(np.pi/4)(0, 2, 1))
+p.inst(CCRY(np.pi/4)(0, 2, 1))
 
 # BC' rotation
-# p.inst(CCRY(np.pi/4)(1, 2, 0))
+p.inst(CCRY(np.pi/4)(1, 2, 0))
 
 wavefunction = qvm.wavefunction(p)
 print(wavefunction)
